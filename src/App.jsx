fcd860fa6f1fa84ce132c438757821a5d4e36867
Sümeyye Danışmaz <smeyyedansmzz@gmail.com>
2024-11-05 20:05:58 +0300

    form

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './reset.css'
 import './App.css'
 import Header from './Header'
+import Form from './Form'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Form />
     </>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,37 +1,67 @@
-import React from "react";
-import './index.css'
-import {
-    BrowserRouter,
-    Switch,
-    Route,
-    NavLink
-} from "react-router-dom";
+import React from 'react'
 
 function Header() {
-
     return (
+        <div className='width'>
 
-        <div>
-            <div className="width">
-                <div>
-                    <h1 className="tecno">Teknolojik Yemekler</h1>
-                </div>
+            <h1 className='tecno'>Teknolojik Yemekler</h1>
 
-                <BrowserRouter>
-                    <Switch>
-                        <Route>
-                            <NavLink to="/" exact>
-                                Anasayfa-
-                            </NavLink>
-
-                            <NavLink to="/siparis" >
-                                Sipariş Oluştur
-                            </NavLink>
-                        </Route>
-                    </Switch>
-                </BrowserRouter>
-            </div>
+            <nav>
+                <ul className='ul'>
+                    <li>Anasayfa</li>
+                    <li>Sipariş Oluştur</li>
+                </ul>
+            </nav>
         </div>
     )
 }
+
 export default Header
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import './index.css'
+// import {
+//     BrowserRouter,
+//     Switch,
+//     Route,
+//     NavLink
+// } from "react-router-dom";
+
+// function Header() {
+
+//     return (
+
+//         <div>
+//             <div className="width">
+//                 <div>
+//                     <h1 className="tecno">Teknolojik Yemekler</h1>
+//                 </div>
+
+//                 <BrowserRouter>
+//                     <Switch>
+//                         <Route>
+//                             <NavLink to="/" exact>
+//                                 Anasayfa-
+//                             </NavLink>
+
+//                             <NavLink to="/siparis" >
+//                                 Sipariş Oluştur
+//                             </NavLink>
+//                         </Route>
+//                     </Switch>
+//                 </BrowserRouter>
+//             </div>
+//         </div>
+//     )
+// }
+// export default Header
