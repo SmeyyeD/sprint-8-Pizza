acf6d0e37bc802a44a7ad72601dad0d0b16216ac
Sümeyye Danışmaz <smeyyedansmzz@gmail.com>
2024-11-09 17:38:32 +0300

    hookslar eklendi

--- src/Form.jsx
+++ src/Form.jsx
@@ -20,45 +20,93 @@ const malzemeler = [
     "Kabak",
 ];
 
-function Form() {
-    const [form, setForm] = useState({ boyut: "", hamur: "", malzemeler: [] });
 
-    // const handleChange = (e) => {
-    //   const { name, value, checked } = e.target;
-    //   const newForm = { ...form, [name]: value };
-    //   setForm(newForm);
-    // };
+function Form() {
+    const [form, setForm] = useState({
+        boyut: "",
+        hamur: "",
+        malzemeler: [],
+        siparisNotu: "",
+        sayac: 1,
+        ekstraTutar: 0,
+        toplamTutar: 85.5,
+    });
+    const [error, setError] = useState({
+        boyutHata: "",
+        hamurHata: "",
+        malzemelerHata: "",
+    });
+
+
+    function hesaplaToplamTutar(yeniMalzemeler, yeniSayac) {
+        const malzemeTutar = yeniMalzemeler.length * 5;
+        const toplamTutar = (85.5 + malzemeTutar) * yeniSayac;
+        setForm((prevForm) => ({
+            ...prevForm,
+            ekstraTutar: malzemeTutar,
+            toplamTutar: toplamTutar,
+        }));
+    }
 
     const handleChange = (e) => {
         const { name, value, checked } = e.target;
+        if (name === "boyut") {
+            value === "Küçük"
+                ? setError({ ...error, boyutHata: "lütfen boyut belirtiniz." })
+                : setError({ ...error, boyutHata: "" });
+        } else if (name === "hamur") {
+            value === "Kalın"
+                ? setError({ ...error, hamurHata: "lütfen hamur belirtiniz." })
+                : setError({ ...error, hamurHata: "" });
+        } else if (form.malzemeler.length >= 9) {
+            checked
+                ? setError({
+                    ...error,
+                    malzemelerHata: "En Fazla 10 malzeme seçebilirsiniz.",
+                })
+                : setError({
+                    ...error,
+                    malzemelerHata: "",
+                });
+        }
 
+        let secilenMalzemeler = [...form.malzemeler];
         if (name === "malzemeler") {
-            let secilenMalzemeler = [];
-
             if (checked) {
-                secilenMalzemeler = [...form.malzemeler, value];
+                secilenMalzemeler.push(value);
             } else {
                 secilenMalzemeler = form.malzemeler.filter(
                     (malzeme) => malzeme !== value
                 );
             }
-
             setForm({ ...form, malzemeler: secilenMalzemeler });
+            hesaplaToplamTutar(secilenMalzemeler, form.sayac);
         } else {
             setForm({ ...form, [name]: value });
         }
     };
 
+    function handleClick(e) {
+        const { name } = e.target;
+        let yeniSayac = form.sayac;
+        if (name === "azalt" && form.sayac > 1) {
+            yeniSayac -= 1;
+        } else if (name === "arttır" && form.sayac < 10) {
+            yeniSayac += 1;
+        }
+        setForm({ ...form, sayac: yeniSayac });
+        hesaplaToplamTutar(form.malzemeler, yeniSayac); // Her arttırma/azaltmada toplamı güncelle
+    }
+
+
     return (
         <div className='display-flex justify-content align-items text-content gap-5rem flex-direction-column padding'>
             <div className="display-flex flex-direction-column gap-1rem font-barlow color-black max-width-35rem margin-0-auto ">
                 {" "}
-                {/* Fiyat-Ürün Bilgisi */}
+
                 <h2>Position Absolute Acı Pizza</h2>
                 <div className="display-flex justify-content-sb align-items-end">
                     <h1>85.50₺</h1>
-                    {/* ne yaptıysam h1 div in alt cızgısını hızzalanmadı. */}
-
                     <div className="display-flex justify-content-end width-yüz100 gap-3rem ">
                         <p>{`${"4.9"}`}</p>
                         <p>{`${"200"}`}</p>
@@ -94,11 +142,11 @@ function Form() {
                             </label>
                         );
                     })}
+                    {error && <p style={{ color: "red" }}>
+                        {error.boyutHata}</p>}
                 </div>
 
                 <div>
-                    {" "}
-
                     <h2 className="height-2rem">
                         Hamur Seç<span className="color-star ">*</span>
                     </h2>
@@ -115,6 +163,8 @@ function Form() {
                             );
                         })}
                     </select>
+                    {error && <p style={{ color: "red" }}>
+                        {error.hamurHata}</p>}
                 </div>
             </div>
 
@@ -123,6 +173,7 @@ function Form() {
                 <div className="display-flex flex-direction-column gap-1rem">
                     <h2>Ek Malzemeler</h2>
                     <p>En Fazla 10 malzeme seçebilirsiniz. 5₺</p>
+                    {error && <p style={{ color: "red" }}>{error.malzemelerHata}</p>}
                 </div>
                 <div className="display-flex justify-content-sb gap-1-5rem wrap">
                     {malzemeler.map((malzeme, ind) => {
@@ -135,42 +186,127 @@ function Form() {
                                     type="checkbox"
                                     name="malzemeler"
                                     value={malzeme}
-                                    checked={form.checked}
+                                    disabled={
+                                        form.malzemeler.lenght >= 10 &&
+                                        !form.malzemeler.includes(malzeme)
+                                    }
                                     onChange={handleChange}
                                 />
                                 {malzeme}
                             </label>
                         );
                     })}
-
                     <label className="flex-basis-9rem"></label>
-                    <div className="display-flex flex-direction-column min-width-35rem min-height-15rem gap-2rem padding">
-                        <label className="font-size font-weight-bold " htmlFor="orderNote" >Sipariş Notu</label>
-                        <input className="min-height-5rem border-radius" type="text" id="orderNote" name="orderNote" value="" placeholder="Siparişine eklemek istediğin bir not var mı? " />
-                    </div>
-                    <div className="display-flex min-width-35rem space-between height-12rem gap-1rem ">
-                        <div className="display-flex min-height-10rem ">
-                            <button className="width-3rem height-3rem background-color-yellow outline-none border-radius">-</button>
-                            <label className="width-3rem height-3rem text-align-center justify-content-center align-items-center font-size border">0</label>
-                            <button className="width-3rem height-3rem background-color-yellow outline-none border-radius ">+</button>
+                </div>
+            </div>
+
+            {/* <div className="display-flex flex-direction-column min-width-35rem margin-0-auto">
+                <label htmlFor="siparisNotu">Sipariş Notu</label>
+                <textarea
+                    className="resize"
+                    name="siparisNotu"
+                    id="siparisNotu"
+                    placeholder="Siparişine eklemek istediğin bir not var mı?"
+                    value={form.siparisNotu}
+                    onChange={handleChange}
+                ></textarea>
+            </div>
+
+            <div className="display-flex min-width-35rem margin-0-auto">
+                <div className="display-flex ">
+                    <button
+                        name="azalt"
+                        disabled={form.sayac < 2}
+                        onClick={handleClick}
+                        className="size-square-3 background-color-button text-align-c fontsize-1-5rem font-weigth-bold cursor"
+                    >
+                        —
+                    </button>
+                    <label
+                        className="size-square-3-3 display-flex justify-content-center align-items-center "
+                        type="text"
+                    >
+                        {form.sayac}
+                    </label>
+                    <button
+                        name="arttır"
+                        disabled={form.sayac > 9}
+                        onClick={handleClick}
+                        className="size-square-3 background-color-button text-align-c fontsize-1-5rem font-weigth-bold cursor"
+                    >
+                        +
+                    </button>
+                </div>
+
+                <div className="width-yüz100">
+                    <div>
+                        <label>Sipariş Toplamı</label>
+                        <div className="display-flex justify-content-sb">
+                            <label>Ekstra Tutar:</label>
+                            <label> {form.ekstraTutar} </label>
                         </div>
-                        <div className="width-yüz100 display-flex flex-direction-column space-between border border-radius-7rem">
-                            <label className="font-weight-bold letter-spacing margin-left height-4rem font-barlow" >Sipariş Toplamı</label>
-                            <div className="display-flex space-around font-barlow font-weight-bold">
-                                <label >Seçimler</label>
-                                <label >20.00₺</label>
-                            </div>
-                            <div className="display-flex space-around font-barlow color-red font-weight-bold">
-                                <label>Toplam</label>
-                                <label >110.50₺</label>
-                            </div>
-                            <button className="width-yüz100 height-3rem background-color-yellow font-size-1rem outline-none border-radius">SİPARİŞ VER</button>
+                        <div className="display-flex justify-content-sb">
+                            <label>Toplam Tutar:</label>
+                            <label> {form.toplamTutar} </label>
                         </div>
                     </div>
+                    <button
+                        onClick={() => hesaplaToplamTutar(form.malzemeler, form.sayac)}
+                        className="max-width-div-18rem background-color-button text-align-c border-none border-radius-0-3rem"
+                    >
+                        Sipariş Ver
+                    </button>
                 </div>
             </div>
         </div>
     );
 }
 
-export default Form
+export default Form; */}
+
+
+            <div className="display-flex flex-direction-column min-width-35rem min-height-15rem gap-2rem padding">
+                <label className="font-size font-weight-bold " htmlFor="orderNote" >Sipariş Notu</label>
+                <input className="min-height-5rem border-radius" type="text" id="orderNote" name="orderNote" value="" placeholder="Siparişine eklemek istediğin bir not var mı? " />
+            </div>
+
+            <div className="display-flex min-width-35rem space-between height-12rem gap-1rem ">
+                <div className="display-flex min-height-10rem ">
+
+                    <button
+                        name="azalt"
+                        disabled={form.sayac < 2}
+                        onClick={handleClick}
+                        className="width-3rem height-3rem background-color-yellow outline-none border-radius">
+                        -  </button>
+
+                    <label
+                        className="width-3rem height-3rem text-align-center justify-content-center align-items-center font-size border"
+                        type="text">{form.sayac}</label>
+
+                    <button
+                        name="arttır"
+                        disabled={form.sayac > 9}
+                        onClick={handleClick}
+                        className="width-3rem height-3rem background-color-yellow outline-none border-radius ">+</button>
+                </div>
+
+                <div className="width-yüz100 display-flex flex-direction-column space-between border border-radius-7rem">
+                    <label className="font-weight-bold letter-spacing margin-left height-4rem font-barlow" >Sipariş Toplamı</label>
+                    <div className="display-flex space-around font-barlow font-weight-bold">
+                        <label >Ekstra Tutar:</label>
+                        <label > {form.ekstraTutar} </label>
+                    </div>
+                    <div className="display-flex space-around font-barlow color-red font-weight-bold">
+                        <label>Toplam Tutar:</label>
+                        <label > {form.toplamTutar} </label>
+                    </div>
+                    <button onClick={() => hesaplaToplamTutar(form.malzemeler, form.sayac)}
+                        className="width-yüz100 height-3rem background-color-yellow font-size-1rem outline-none border-radius">SİPARİŞ VER</button>
+                </div>
+            </div>
+        </div >
+    );
+}
+
+export default Form 
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,8 +3,8 @@ import './App.css'
 import './Form.jsx'
 function Header() {
     return (
-        <div className="display-flex flex-direction-column justify-content align-items max-width-div-header max-height-div-header background-color-header color-white ">
-            <h1 className="font-barlow fontsize-header-h1 ">Teknolojik Yemekler</h1>
+        <div className="display-flex flex-direction-column justify-content align-items max-width-div-header max-height-div-header background-color-header color-white gap-2rem ">
+            <h1 className="font-barlow fontsize-header-h1">Teknolojik Yemekler</h1>
             <nav>
                 <ul className="fontsize-header-ul display-flex gap-1rem list-style-type-none">
                     <li>
